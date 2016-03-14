db69b7c5b3d60320208fd0d092c2958b90f4d2d7
Nora Hilda De los Reyes <norahildadelosreyes@MacBook-Air-de-Nora.local>
2016-03-14 20:35:24 +0000

    tableviewcell commit

---
+++ ParseStarterProject/ProductosTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  ProductosTableViewCell.swift
+//  Catalogo
+//
+//  Created by Nora Hilda De los Reyes on 14/03/16.
+//  Copyright © 2016 Parse. All rights reserved.
+//
+
+import UIKit
+
+class ProductosTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var productoImageView: UIView!
+    @IBOutlet weak var nombreLabel: UILabel!
+    @IBOutlet weak var descripcionLabel: UILabel!
+    @IBOutlet weak var cantidadLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
