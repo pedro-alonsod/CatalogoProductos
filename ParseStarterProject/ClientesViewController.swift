1883fbd94269af8f2d4f55cfc7e5df30c67c05f7
Pedro Alonso <norahildadelosreyes@macbook-air-de-nora.local>
2016-03-18 16:21:18 +0000

    Mails from app done maybe is better than msgs

--- ParseStarterProject/ClientesViewController.swift
+++ ParseStarterProject/ClientesViewController.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 import Parse
+import MessageUI
 
 
-class ClientesViewController: UIViewController {
+class ClientesViewController: UIViewController, MFMailComposeViewControllerDelegate {
 
     
     var arrayProductos: [PFObject] = []
@@ -166,5 +167,38 @@ class ClientesViewController: UIViewController {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     
+    @IBAction func mandarMensajeTapped(sender: UIButton) {
+        
+        let mailComposeViewController = configuredMailComposeViewController()
+        if MFMailComposeViewController.canSendMail() {
+            self.presentViewController(mailComposeViewController, animated: true, completion: nil)
+        } else {
+            self.showSendMailErrorAlert()
+        }
+    }
+    
+    
+    func configuredMailComposeViewController() -> MFMailComposeViewController {
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self // Extremely important to set the --mailComposeDelegate-- property, NOT the --delegate-- property
+        
+        mailComposerVC.setToRecipients(["pedro.alonsod@gmail.com"])
+        mailComposerVC.setSubject("Sending you an in-app e-mail...")
+        mailComposerVC.setMessageBody("Productos", isHTML: false)
+        
+        return mailComposerVC
+    }
+    
+    func showSendMailErrorAlert() {
+        let sendMailErrorAlert = UIAlertView(title: "Could Not Send Email", message: "Your device could not send e-mail.  Please check e-mail configuration and try again.", delegate: self, cancelButtonTitle: "OK")
+        sendMailErrorAlert.show()
+    }
+    
+    // MARK: MFMailComposeViewControllerDelegate
+    
+    func mailComposeController(controller: MFMailComposeViewController!, didFinishWithResult result: MFMailComposeResult, error: NSError!) {
+        controller.dismissViewControllerAnimated(true, completion: nil)
+        
+    }
 
 }
