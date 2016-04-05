da5e4f422aca82ede9bbc13a7bfd7290a1203829
Pedro Alonso <norahildadelosreyes@macbook-air-de-nora.local>
2016-04-05 17:08:44 -0500

    Remember this a the god damn end always   arrayMensajes.removeAtIndex(indexPath.row)   tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Left)
      tableView.reloadData()
    
    after the parse has done everything

--- ParseStarterProject/VerMensajesViewController.swift
+++ ParseStarterProject/VerMensajesViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import Parse
+
 
 class VerMensajesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     
+
+    var arrayMensajes: [PFObject] = []
     
     @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    let nombre = "usuarioDelMensaje", userName = "username", descripcion = "mensajeDescripcion", precio = "precio", cantidad =  "cantidad", pedir = "pedir"
     
     
     @IBAction func salirTapped(sender: UIButton) {
@@ -24,6 +31,31 @@ class VerMensajesViewController: UIViewController, UITableViewDelegate, UITableV
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        
+        let queryMessages = PFQuery(className: "Mensajes")
+        
+        queryMessages.whereKey("visto", equalTo: false)
+        queryMessages.limit = 200
+        queryMessages.addDescendingOrder("createdAt")
+        queryMessages.includeKey("usuarioDelMensaje")
+        
+        do {
+            
+            arrayMensajes = try queryMessages.findObjects()
+            
+        } catch let error {
+            
+            print(error)
+        }
+        
+        print(arrayMensajes.count)
+        print("we got them \(self.arrayMensajes.count)")
+        print(arrayMensajes[0].valueForKey("mensajeDescripcion")!)
+        print(arrayMensajes[0].valueForKey("visto")!)
+        
+
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,6 +63,32 @@ class VerMensajesViewController: UIViewController, UITableViewDelegate, UITableV
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewWillAppear(animated: Bool) {
+        
+ 
+//        queryMessages.findObjectsInBackgroundWithBlock {
+//            
+//            (objects: [PFObject]?, error: NSError? ) -> Void in
+//            
+//            if error == nil {
+//                
+//                self.arrayMensajes = objects!
+//                print("we got them \(self.arrayMensajes.count)")
+//                
+//                print("prueba \(self.arrayMensajes[0].valueForKey(self.nombre)!.valueForKey(self.userName)!)")
+//                print(self.arrayMensajes[0].valueForKey(self.nombre)!.valueForKey(self.userName)! as? String)
+//                
+//                
+//                
+//            } else {
+//                
+//                print(error?.localizedDescription)
+//                
+//                
+//            }
+//        }
+//        
+    }
 
     /*
     // MARK: - Navigation
@@ -48,17 +106,19 @@ class VerMensajesViewController: UIViewController, UITableViewDelegate, UITableV
         return 1
     }
     
+    
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         print("this second")
-        return 5
+        return arrayMensajes.count
     }
     
     func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         
-        if section == 1 {
+        if section == 0 {
             
-            return "Productos"
+            return "Mensajes Nuevos"
             
         } else {
             
@@ -71,11 +131,13 @@ class VerMensajesViewController: UIViewController, UITableViewDelegate, UITableV
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         
-        let nombre = "nombre", foto = "Foto", descripcion = "descripcion", precio = "precio", cantidad =  "cantidad", pedir = "pedir"
         
-        
-        let cell = tableView.dequeueReusableCellWithIdentifier("VerMensajesCell", forIndexPath: indexPath)
-        
+        let cell = tableView.dequeueReusableCellWithIdentifier("VerMensajesCell", forIndexPath: indexPath) as! VerMensajesTableViewCell
+        //print("prueba \(arrayMensajes[indexPath.row].valueForKey(nombre)!.valueForKey(userName)!)")
+        //print(arrayMensajes[indexPath.row].valueForKey("usuarioDelMensaje")!.valueForKey("Username")! as? String)
+        print("this fucking index is driving me crazy \(indexPath.row) and this \(arrayMensajes.count)")
+        cell.usernameMessagesLabel.text = arrayMensajes[indexPath.row].valueForKey("usuarioDelMensaje")!.valueForKey("username")! as? String
+        cell.massageLabel.text = arrayMensajes[indexPath.row].valueForKey("mensajeDescripcion")! as? String
         
         
         return cell
@@ -98,6 +160,35 @@ class VerMensajesViewController: UIViewController, UITableViewDelegate, UITableV
         if editingStyle == .Delete {
             // Delete the row from the data source
             
+            print("index of this row \(indexPath.row)")
+            print("object at index \(arrayMensajes[indexPath.row])")
+            let queryVisto = PFQuery(className: "Mensajes")
+            
+            print(arrayMensajes[indexPath.row].objectId!)
+            print(indexPath.row)
+            
+            queryVisto.getObjectInBackgroundWithId(arrayMensajes[indexPath.row].objectId!) {
+                
+                (objectSeen: PFObject?, error: NSError?) -> Void in
+                
+                if error == nil {
+                    
+                    print(objectSeen!["visto"])
+                    print(objectSeen!.objectId!)
+                    objectSeen!["visto"] = true
+                    objectSeen!.saveInBackground()
+                    
+                } else {
+                    
+                    print(error)
+                }
+                
+            }
+            
+            arrayMensajes.removeAtIndex(indexPath.row)
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Left)
+            
+            tableView.reloadData()
             
             
         } else if editingStyle == .Insert {
---
+++ VerMensajesTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  VerMensajesTableViewCell.swift
+//  Catalogo
+//
+//  Created by Nora Hilda De los Reyes on 05/04/16.
+//  Copyright © 2016 Parse. All rights reserved.
+//
+
+import UIKit
+
+class VerMensajesTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var usernameMessagesLabel: UILabel!
+    @IBOutlet weak var massageLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
